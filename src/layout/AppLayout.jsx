100409c76505464879695b201ad209d1a216eba8
Taeyoung_Kim <154067698+teddybearkim@users.noreply.github.com>
2024-04-07 00:28:52 +0900

    Add files via upload

---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./AppLayout.style.css";
+
+const AppLayout = () => {
+  return (
+    <div className="all">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container fluid>
+          <Navbar.Brand href="#" className="hi">
+            <img
+              width={100}
+              src="https://images.ctfassets.net/4cd45et68cgf/4nBnsuPq03diC5eHXnQYx/d48a4664cdc48b6065b0be2d0c7bc388/Netflix-Logo.jpg"
+            ></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to = "/" className="nav-list">Home</Link>
+              <Link to = "Movies" className="nav-list">Link</Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
