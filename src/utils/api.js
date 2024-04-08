e55f2e1718787649554ed298bd9d17c5b2a3f244
Taeyoung_Kim <154067698+teddybearkim@users.noreply.github.com>
2024-04-08 18:27:31 +0900

    Add files via upload

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(API_KEY);
+const REACT_APP_API_KEY = process.env.REACT_APP_API_KEY;
+
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
     Accept: "application/json",
-    Authorization: `Bearer ${API_KEY}`,
+    Authorization: `Bearer ${REACT_APP_API_KEY}`,
   },
 });
 // 요청 인터셉터 추가하기
